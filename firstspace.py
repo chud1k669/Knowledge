19c33477b1f171ec6fb3f1e33f584a522ce43257
chud1k669 <knop.2000@mail.ru>
2024-11-12 17:00:00 +0300

    day2

--- firstspace.py
+++ firstspace.py
@@ -1,3 +1,3 @@
 a = input()
-b = a.replace(" " , " \'", 1)
-print(b.replace(" " , " \""))
+b = a.replace(" " , "'", 1)
+print(b.replace(" " , '"'))
---
+++ raw.py
@@ -0,0 +1,2 @@
+a = r'C:\WINDOWS\System32\drivers\etc\hosts.'
+print(a)
